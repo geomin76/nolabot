a30a68d89be8bff2860bf286d8ac5171058f83bc
geomin76 <geom@vt.edu>
2020-08-04 21:20:44 -0400

    Cleanup and creating a service file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tweepy
 import twint
 import logging
 import re
+import service
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,7 +16,6 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
-
 
 app = Flask(__name__)
 
@@ -47,40 +47,12 @@ def getTweets():
     logging.info("Saving tweets to " + user + ".txt file")
     file1 = open(user + ".txt", "w")
     for i in tweets:
-        parsedStr = remove_emoji(i.tweet)
-        parsedStr = re.sub(r"http\S+", "", parsedStr)
-        parsedStr = re.sub(r'pic.twitter.com/[\w]*', "", parsedStr)
-        parsedStr = re.sub(r'^https?:\/\/.*[\r\n]*', '', parsedStr, flags=re.MULTILINE)
-        parsedStr = re.sub(r'@[\w]*', '', parsedStr)
-        parsedStr = re.sub(r'…', '', parsedStr)
+        parsedStr = service.parsingString(i.tweet)
         if parsedStr.strip():
             file1.write(parsedStr + "\n")
     file1.close()
-    
     return "Tweets saved on " + user + ".txt"
 
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002500-\U00002BEF"  # chinese char
-                               u"\U00002702-\U000027B0"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # dingbats
-                               u"\u3030"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
 
 
 if __name__ == '__main__':
